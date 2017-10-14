f063ddc789d9e609314a04f07cb7aff14374ccff
Ivan Pushkin <pushkin13@bk.ru>
2017-10-15 02:13:22 +0300

    Create tests

--- points_sequence_generator_test.go
+++ points_sequence_generator_test.go
@@ -1,16 +1,43 @@
 package imgio
 
 import (
-	"fmt"
 	"image"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func Test_SimplePointsSequenceGenerator_Current(t *testing.T) {
-	g := NewSimplePointsSequenceGenerator(image.Rect(-10, -10, -1, -1))
-	for i := 0; i < g.rect.Size().X*g.rect.Size().Y; i++ {
-		p := g.Current()
-		fmt.Printf("%4d %s\n", i, p)
-		g.Next()
+func Test_SimplePointsSequenceGenerator_Valid_ReturnsTrue(t *testing.T) {
+	g := &SimplePointsSequenceGenerator{
+		rect:   image.Rect(0, 0, 1, 1),
+		cursor: 0,
+	}
+	require.True(t, g.Valid())
+}
+
+func Test_SimplePointsSequenceGenerator_Valid_RetursFalse(t *testing.T) {
+	g := &SimplePointsSequenceGenerator{
+		rect:   image.Rect(-1, -1, 1, 1),
+		cursor: 4,
+	}
+	require.False(t, g.Valid())
+}
+
+func Test_SimplePointsSequenceGenerator_Current_ReturnsValidCoordinatesByIndex(t *testing.T) {
+	g := &SimplePointsSequenceGenerator{
+		rect:   image.Rect(-5, -5, 1, 1),
+		cursor: 0,
+	}
+
+	tests := map[uint64]image.Point{
+		0: image.Point{-5, -5},
+		4: image.Point{-1, -5},
+		6: image.Point{-5, -4},
+		9: image.Point{-2, -4},
+	}
+
+	for cursor, point := range tests {
+		g.cursor = cursor
+		require.Equal(t, point, g.Current())
 	}
 }
