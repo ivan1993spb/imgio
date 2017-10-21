cab52c97ec430bda8f1965abc40e4d88ccfccba0
Ivan Pushkin <pushkin13@bk.ru>
2017-10-21 04:12:01 +0300

    Create cli version

---
+++ cmd/imgio/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"image"
+	"image/draw"
+	"image/png"
+	"io"
+	"log"
+	"os"
+	"sort"
+
+	"github.com/ivan1993spb/imgio"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{}
+
+	app.Commands = []cli.Command{
+		{
+			Name: "encode",
+			Action: func(c *cli.Context) error {
+				img := imgio.NewImage(
+					image.NewRGBA(image.Rect(0, 0, 10, 10)),
+					imgio.NewSimplePointsSequenceGenerator(image.Rect(0, 0, 10, 10)),
+					imgio.SimplePoint32ReadWriter{},
+				)
+				n, err := io.Copy(img, os.Stdin)
+				log.Println(n, err)
+
+				err = png.Encode(os.Stdout, img)
+				log.Println(err)
+				return nil
+			},
+		},
+		{
+			Name: "decode",
+			Action: func(c *cli.Context) error {
+				i, err := png.Decode(os.Stdin)
+				log.Println(err)
+
+				cimg := image.NewRGBA(i.Bounds())
+				draw.Draw(cimg, i.Bounds(), i, image.Point{}, draw.Over)
+
+				img := imgio.NewImage(
+					cimg,
+					imgio.NewSimplePointsSequenceGenerator(image.Rect(0, 0, 10, 10)),
+					imgio.SimplePoint32ReadWriter{},
+				)
+				n, err := io.Copy(os.Stdout, img)
+				log.Println()
+				log.Println(n, err)
+
+				return nil
+			},
+		},
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	app.Run(os.Args)
+}
